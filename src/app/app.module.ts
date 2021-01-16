ebe33e0c9cd03a7f29bd9b7c631614fe4c9514e9
prerna <chaturvediprerna03@gmail.com>
2021-01-16 23:10:26 +0530

    week3 task1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,7 +58,8 @@ import { LoginComponent } from './login/login.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatFormFieldModule, 
+    MatFormFieldModule,
+    MatSliderModule,
     MatInputModule,
     MatCheckboxModule,
     MatSelectModule,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Params } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSliderChange } from '@angular/material';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,19 +19,41 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
-  
-  constructor(private dishservice: DishService, private route: ActivatedRoute, private location: Location) { }
+  commentForm:FormGroup;
+  comment:Comment[];
+  rating: number;
+  @ViewChild('fform') feedbackFormDirective;
+  constructor(private fb:FormBuilder, private dishservice: DishService, private route: ActivatedRoute, private location: Location) {
+      this.createForm();
+  }
 
   ngOnInit(): void {
+    this.rating = 5;
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
-  
+    
   }
+   
+  updateSetting(event: MatSliderChange) {
+    console.log(event);
+  }
+
+  createForm():void{
+     this.commentForm = this.fb.group({
+       name:['',[Validators.required, Validators.minLength(10), Validators.maxLength(25)]],
+       comment: ['',[Validators.required, Validators.minLength(10)]]
+     })
+  }
+
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
     this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
-
+  handleSubmit(){
+    console.log(this.commentForm.value);
+    // this.comment.push()
+    this.commentForm.reset();
+  }  
 }
